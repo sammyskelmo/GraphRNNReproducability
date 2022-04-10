a082ff12cc8cf7c2614cbaa9137bb7cdbc4320f8
JasperSZM <zmsongzm@gmail.com>
2022-04-10 13:46:24 +0100

    Training setting

--- args.py
+++ args.py
@@ -23,7 +23,7 @@ class Args():
         # self.graph_type = 'caveman_small'
         # self.graph_type = 'caveman_small_single'
         # self.graph_type = 'community4'
-        self.graph_type = 'grid'
+        # self.graph_type = 'grid'
         # self.graph_type = 'grid_small'
         # self.graph_type = 'ladder_small'
 
@@ -31,7 +31,7 @@ class Args():
         # self.graph_type = 'enzymes_small'
         # self.graph_type = 'barabasi'
         # self.graph_type = 'barabasi_small'
-        # self.graph_type = 'citeseer'
+        self.graph_type = 'citeseer'
         # self.graph_type = 'citeseer_small'
 
         # self.graph_type = 'barabasi_noise'
@@ -62,13 +62,13 @@ class Args():
         self.num_layers = 4
 
         ### training config
-        self.num_workers = 2 # num workers to load data, default 4
+        self.num_workers = 4 # num workers to load data, default 4
         self.batch_ratio = 32 # how many batches of samples per epoch, default 32, e.g., 1 epoch = 32 batches
         self.epochs = 3000 # now one epoch means self.batch_ratio x batch_size
-        self.epochs_test_start = 100
-        self.epochs_test = 100
+        self.epochs_test_start = 500
+        self.epochs_test = 500
         self.epochs_log = 10
-        self.epochs_save = 100
+        self.epochs_save = 1000
 
         self.lr = 0.003
         self.milestones = [400, 1000]
